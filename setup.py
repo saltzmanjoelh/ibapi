b099a4f302093ca012e0e69a05770e4003043a5d
Joel Saltzman <sorghum-fifers.08@icloud.com>
2024-06-02 21:12:50 +0000

    Updated package

--- setup.py
+++ setup.py
@@ -1,7 +1,23 @@
-from setuptools import setup, find_packages
+"""
+Copyright (C) 2019 Interactive Brokers LLC. All rights reserved. This code is subject to the terms
+ and conditions of the IB API Non-Commercial License or the IB API Commercial License, as applicable.
+"""
+#from distutils.core import setup
+from setuptools import setup
+from ibapi import get_version_string
+
+import sys
+
+if sys.version_info < (3,1):
+    sys.exit("Only Python 3.1 and greater is supported") 
 
 setup(
     name='ibapi',
-    version='0.1',
-    packages=find_packages(),
+    version=get_version_string(),
+    packages=['ibapi'],
+    url='https://interactivebrokers.github.io/tws-api',
+    license='IB API Non-Commercial License or the IB API Commercial License',
+    author='IBG LLC',
+    author_email='dnastase@interactivebrokers.com',
+    description='Python IB API'
 )
